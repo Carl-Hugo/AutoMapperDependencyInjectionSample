b9e9e2efb6dcc47692983456a8e8e2144406dd5d
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-12-31 15:38:28 -0500

    Cleanup

--- AutoMapperDependencyInjectionSample/Startup.cs
+++ AutoMapperDependencyInjectionSample/Startup.cs
@@ -13,15 +13,11 @@ namespace AutoMapperDependencyInjectionSample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Auto Mapper
             services.AddAutoMapper(typeof(Startup).Assembly);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
